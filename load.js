744abc9707e446b5fc25b5e87a60c5565a36041b
Tanvir <132451362+optco@users.noreply.github.com>
2026-06-12 13:25:18 +0200

    Update load.js

--- load.js
+++ load.js
@@ -63,7 +63,7 @@
       const footer = document.createElement('div');
       footer.id = 'pl-footer';
       footer.innerHTML = `
-        <a href="https://theattn.com/" target="_blank" rel="noopener">
+        <a href="https://theattn.com/" target="_blank" rel="noopener" title="Contact me">
          <img src="https://theattn.com/favicons/favicon-32x32.png" alt="theattn" class="status-text">
         </a>
       `;
